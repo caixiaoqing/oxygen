cdba9ef04b775004283328356eb485dc765e25aa
caixiaoqing <hermione.cai@gmail.com>
2016-10-23 20:50:12 +0800

    send mail by php

--- template/sendemail.php
+++ template/sendemail.php
@@ -3,16 +3,21 @@ $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email'])); 
 $subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'email@email.com';//replace with your email
+$to   		= 'support@pst-sg.com';//replace with your email
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
 $headers[] = "Content-type: text/plain; charset=iso-8859-1";
 $headers[] = "From: {$name} <{$from}>";
-$headers[] = "Reply-To: <{$from}>";
+$headers[] = "Reply-To: {$from}";
 $headers[] = "Subject: {$subject}";
 $headers[] = "X-Mailer: PHP/".phpversion();
 
-mail($to, $subject, $message, $headers);
+$send = mail($to, $subject, $message, implode("\r\n", $headers));
 
+if ($send) {
+    header("Refresh:2; url=http://pst-sg.com#contact");
+    echo "Thank you for contacting us. We will be in touch with you very soon.";
+}
+else { echo "ERROR! Please go back and try again."; }
 die;
